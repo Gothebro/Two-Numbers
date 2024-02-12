a388c57621a66a79db5932c67a6bd2119c7541ae
Jon <jbeqiri@iwu.edu>
2024-02-12 09:47:13 -0600

    app

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+OPERATIONS = ["+", "-", "*", "/"]
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
 
+
+@app.route('/twonumber', methods=['GET', 'POST'])
+def calc():  # put application's code here
+    output = None
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+        if operation == "+":
+            output = num1 + num2
+        elif operation == "-":
+            output = num1 - num2
+        elif operation == "*":
+            output = num1 * num2
+        elif operation == "/":
+            if num2 == 0:
+                output = 0
+            output = num1 / num2
+    return render_template('output.html', OPERATION=operation, num1=num1, num2=num2)
 
 if __name__ == '__main__':
     app.run()
