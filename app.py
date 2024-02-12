acd86aa6d4d3f977bfb73473656cf0f60cf0cbb6
Griffin Owens <Gothebro@gmail.com>
2024-02-12 10:33:08 -0600

    made modifications to make the program work

--- app.py
+++ app.py
@@ -4,29 +4,31 @@ app = Flask(__name__)
 
 OPERATIONS = ["+", "-", "*", "/"]
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", operations=OPERATIONS)
 
 
-@app.route('/twonumber', methods=['GET', 'POST'])
+@app.route('/calculate', methods=['POST'])
 def calc():  # put application's code here
     output = None
-    if request.method == 'POST':
-        num1 = request.form['num1']
-        num2 = request.form['num2']
-        operation = request.form['operation']
-        if operation == "+":
-            output = num1 + num2
-        elif operation == "-":
-            output = num1 - num2
-        elif operation == "*":
-            output = num1 * num2
-        elif operation == "/":
-            if num2 == 0:
-                output = 0
-            output = num1 / num2
-    return render_template('output.html', OPERATION=operation, num1=num1, num2=num2)
+    num1 = request.form.get("num1")
+    num2 = request.form.get("num2")
+    operation = request.form.get("operation")
+    if operation == "+":
+        output = int(num1) + int(num2)
+    elif operation == "-":
+        output = int(num1) - int(num2)
+    elif operation == "*":
+        output = int(num1) * int(num2)
+    elif operation == "/":
+        if num2 == 0:
+            output = 0
+        output = int(num1) / int(num2)
+    return render_template('output.html', operation=request.form.get("operation"),
+                           num1=request.form.get("num1"), num2=request.form.get("num2"), output=output)
+
 
 if __name__ == '__main__':
     app.run()
